526d03a993aa892b355095b1b012ff79b275022b
burner-account-sh <148162856+burner-account-sh@users.noreply.github.com>
2023-10-17 21:04:24 +0300

    Add files via upload

---
+++ wine-ge.sh
@@ -0,0 +1,8 @@
+mkdir ~/.var/app/io.github.vinegarhq.Vinegar/wine
+cd ~/.var/app/io.github.vinegarhq.Vinegar/wine
+wget https://github.com/GloriousEggroll/wine-ge-custom/releases/download/GE-Proton8-20/wine-lutris-GE-Proton8-20-x86_64.tar.xz
+tar -xf wine-lutris-GE-Proton8-20-x86_64.tar.xz
+rm wine-lutris-GE-Proton8-20-x86_64.tar.xz
+mv lutris-GE-Proton8-20-x86_64 wine-ge
+sleep 10 | echo wineroot = "$HOME/.var/app/io.github.vinegarhq.Vinegar/wine/wine-ge"
+flatpak run io.github.vinegarhq.Vinegar
